e24199476677ea8bb0a769b52b3a809e54382180
Felix Hong <honghuuphuc@gmail.com>
2024-05-18 18:32:08 +1000

    Update pipeline.yml

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}disabled>
+        <button onClick={() => setCount((count) => count + 1)}>
           {count} is {checkEven}
         </button>
         <p>
