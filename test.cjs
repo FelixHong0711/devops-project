290897f2861797e77496cd3f07b3d804ee93f793
Felix Hong <honghuuphuc@gmail.com>
2024-05-20 16:52:22 +1000

    Update content

--- test.cjs
+++ test.cjs
@@ -1,42 +1,53 @@
-const { Builder, By, Key, until } = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
 
 async function runTest() {
   const options = new chrome.Options();
   // options.addArguments("--no-sandbox");
-  options.setChromeBinaryPath('/usr/bin/chromium-browser');
+  options.setChromeBinaryPath("/usr/bin/chromium-browser");
   // options.addArguments("--disable-dev-shm-usage");
   options.addArguments("--headless");
-  const driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+  const driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(options)
+    .build();
 
   try {
-    await driver.get('http://localhost:8000/');
-    
-    await driver.sleep(800);
+    // The port configured in docker
+    await driver.get("http://localhost:8000/");
 
+    await driver.sleep(800);
+    // I will perform 2 test, first is if button clickable, then I will check the content (count) of button,
+    // if not clickable -> display error immediately
     // Find the button element
-    const button = await driver.wait(until.elementLocated(By.css('button')), 5000);
+    const button = await driver.wait(
+      until.elementLocated(By.css("button")),
+      5000,
+    );
 
     // Check if the button is clickable
     if (await button.isEnabled()) {
       await driver.sleep(800);
       // Click the button
       await button.click();
-      await driver.wait(async () => {
-        const buttonText = await button.getText();
-        return buttonText.includes('1');}, 1000);
 
-      await driver.sleep(1000);
-      
-      // Output success message
-      console.log('Button clicked successfully and count is working well!');
-      console.log('All the tests have been satisfied!');
+      await driver.sleep(800);
+      // Get the button text
+      const buttonText = await button.getText();
+
+      // Check if the count is working well
+      if (buttonText.includes("1")) {
+        console.log("Button clicked successfully!");
+        console.log("Count is working well!");
+      } else {
+        throw new Error("Count is not working well!");
+      }
     } else {
       // Output a message if the button is not clickable
-      throw new Error('Button is not clickable!');
+      throw new Error("Button is not clickable!");
     }
   } catch (error) {
-    console.error('Test failed:', error);
+    console.error("Test failed:", error);
     throw error;
   } finally {
     await driver.quit();
